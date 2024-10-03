1da0589e2b7793991581fdf325736a18a0f313a0
Kenta Kubo <601636+kkebo@users.noreply.github.com>
2024-10-04 03:48:16 +0900

    test: fix FileIteratorTests for WASI

--- Tests/SwiftFormatTests/Utilities/FileIteratorTests.swift
+++ Tests/SwiftFormatTests/Utilities/FileIteratorTests.swift
@@ -17,11 +17,24 @@ final class FileIteratorTests: XCTestCase {
     try touch("project/real2.swift")
     try touch("project/.hidden.swift")
     try touch("project/.build/generated.swift")
+    #if !os(WASI)  // FIXME: Remove this #if
     try symlink("project/link.swift", to: "project/.hidden.swift")
+    #endif
   }
 
   override func tearDownWithError() throws {
+    #if os(WASI)  // WASI doesn't support recursive FileManager.removeItem
+    try FileManager.default.removeItem(at: tmpURL("project/real1.swift"))
+    try FileManager.default.removeItem(at: tmpURL("project/real2.swift"))
+    try FileManager.default.removeItem(at: tmpURL("project/.hidden.swift"))
+    try FileManager.default.removeItem(at: tmpURL("project/.build/generated.swift"))
+    // FIXME: try FileManager.default.removeItem(at: tmpURL("project/link.swift"))
+    try FileManager.default.removeItem(at: tmpURL("project/.build/"))
+    try FileManager.default.removeItem(at: tmpURL("project/"))
     try FileManager.default.removeItem(at: tmpdir)
+    #else
+    try FileManager.default.removeItem(at: tmpdir)
+    #endif
   }
 
   func testNoFollowSymlinks() {
@@ -33,11 +46,17 @@ final class FileIteratorTests: XCTestCase {
 
   func testFollowSymlinks() {
     let seen = allFilesSeen(iteratingOver: [tmpdir], followSymlinks: true)
+    #if os(WASI)
+    XCTAssertEqual(seen.count, 2)
+    #else
     XCTAssertEqual(seen.count, 3)
+    #endif
     XCTAssertTrue(seen.contains { $0.hasSuffix("project/real1.swift") })
     XCTAssertTrue(seen.contains { $0.hasSuffix("project/real2.swift") })
+    #if !os(WASI)  // FIXME: Remove this #if
     // Hidden but found through the visible symlink project/link.swift
     XCTAssertTrue(seen.contains { $0.hasSuffix("project/.hidden.swift") })
+    #endif
   }
 
   func testTraversesHiddenFilesIfExplicitlySpecified() {
@@ -51,12 +70,14 @@ final class FileIteratorTests: XCTestCase {
   }
 
   func testDoesNotFollowSymlinksIfFollowSymlinksIsFalseEvenIfExplicitlySpecified() {
+    #if !os(WASI)  // FIXME: Remove this #if
     // Symlinks are not traversed even if `followSymlinks` is false even if they are explicitly
     // passed to the iterator. This is meant to avoid situations where a symlink could be hidden by
     // shell expansion; for example, if the user writes `swift-format --no-follow-symlinks *`, if
     // the current directory contains a symlink, they would probably *not* expect it to be followed.
     let seen = allFilesSeen(iteratingOver: [tmpURL("project/link.swift")], followSymlinks: false)
     XCTAssertTrue(seen.isEmpty)
+    #endif
   }
 }
 
@@ -73,12 +94,16 @@ extension FileIteratorTests {
       at: fileURL.deletingLastPathComponent(),
       withIntermediateDirectories: true
     )
+    #if os(WASI)  // WASI doesn't support FileManager.createFile because it doesn't support atomic
+    try Data().write(to: fileURL)
+    #else
     struct FailedToCreateFileError: Error {
       let url: URL
     }
     if !FileManager.default.createFile(atPath: fileURL.path, contents: Data()) {
       throw FailedToCreateFileError(url: fileURL)
     }
+    #endif
   }
 
   /// Create a symlink between files or directories in the test's temporary space.
